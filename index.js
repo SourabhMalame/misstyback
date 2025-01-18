ae697897832eca4bb9a2d83728585d1862985319
Sourabh <sourabhmalame@gmail.com>
2025-01-18 21:59:05 +0530

    small up

--- index.js
+++ index.js
@@ -16,8 +16,23 @@ connectDb();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(requestLogger);
-server.use(cors());
 
+const allowedDomains = ['https://misstyback.vercel.app/', 'http://localhost:5173/'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    if (allowedDomains.indexOf(origin) !== -1) {
+      callback(null, true);  // Allow the request if the origin is in the allowedDomains list
+    } else {
+      callback(new Error('Not allowed by CORS'));  // Block the request if the origin is not allowed
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 server.use("/api", app);
 
 const PORT = process.env.PORT || 5001;
