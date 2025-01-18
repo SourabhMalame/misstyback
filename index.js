d3eb5b5c46eda17cc9a89bcf9ea0b92ad1e1b233
Sourabh <sourabhmalame@gmail.com>
2025-01-18 22:17:11 +0530

    set

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.use(express.urlencoded({ extended: true }));
 server.use(requestLogger);
 
 const corsOptions = {
-  origin: ['http://localhost:5173/', 'https://misstyback.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://misstyback.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true // Allow cookies or authorization headers
 };
